847483e56450629f1c38ee39219bdbddeebc5b96
JeffMcHugh <Jeff.McHugh@gmail.com>
2017-03-18 13:22:49 -0400

    Update WelcomeTableMonthV2.js

--- WelcomeTableMonthV2.js
+++ WelcomeTableMonthV2.js
@@ -39,20 +39,9 @@ function retrieveData(passvar,mm,mon){
       //console.log(topCtyTable);
       }
   };
+  $.get(url,expdata,callback);
 }
-      
-var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'sidebar.html');
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      document.getElementById('ajax').innerHTML = xhr.responseText;
-    }
-  };
-
-  function sendAJAX() {
-    xhr.send();
-    document.getElementById('load').style.display = 'none';
-  }      
+ 
       
      /* var CalcValue = byValue.slice();
       CalcValue[[0][5]]="Unit Price";
