b2340999870b80e2e1513833f68805bee50e29b1
JeffMcHugh <Jeff.McHugh@gmail.com>
2017-03-19 08:58:37 -0400

    Update WelcomeTableMonthV2.js

--- WelcomeTableMonthV2.js
+++ WelcomeTableMonthV2.js
@@ -55,8 +55,8 @@ function retrieveData(passvar,mm,mon){
 	val5=topCtyTable["Dec"][1][3];
         $(document).ready(function(){
           $( "#commname" ).text(topCtyTable[mon][1][0]).toLocaleString();
-          var markup = "<tr><td class=col1>" + country + "</td><td class=midcol>" + val1 + "</td><td class=midcol>" + val2 
-	               "</td><td class=midcol>" + val3 "</td><td class=midcol>" + val4 + "</td><td class=midcol>" + val5 +
+          var markup = "<tr><td class=col1>" + country + "</td><td class=midcol>" + val1 + "</td><td class=midcol>" + val2 +
+	               "</td><td class=midcol>" + val3 + "</td><td class=midcol>" + val4 + "</td><td class=midcol>" + val5 +
 		       "</td></tr>";
           $("table tbody").append(markup);
         });  
