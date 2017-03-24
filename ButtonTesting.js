5d3138fa1200568a3a4b47748b10cdc79aae8450
JeffMcHugh <Jeff.McHugh@gmail.com>
2017-03-24 11:54:35 -0400

    Update ButtonTesting.js

--- ButtonTesting.js
+++ ButtonTesting.js
@@ -15,28 +15,31 @@
 </div>  */
 
 function gethsnumbers(year,monthStr){
-				$.get("https://api.census.gov/data/timeseries/intltrade/exports", {
-					get: "E_COMMODITY,E_COMMODITY_SDESC",
-					YEAR: year,
-					MONTH: monthStr,
-					COMM_LVL: "HS10",
-					key: "63550916d57e686361cb2c21a3634dd765e01e28",
-				}, function(data) {
-					if(!data) {
-						// Nothing found, try again with previous month
-						console.log("No HS numbers found");
-					}
-					else {
-						var AllHS = data.slice(0);
-						var numcodes = AllHS.length;
-						var hscodelist = Array(numcodes);
-						var i;
-    						for (i=1;i<=numcodes;i++){
-      						hscodelist[[i-1][0]] = AllHS[[i][0]];
-      						hscodelist[[i-1][1]] = AllHS[[i][1]];
-    						}	
-					}
-				});
+	var url2="https://api.census.gov/data/timeseries/intltrade/exports";
+  	var expdata2 = {
+	  	get:"E_COMMODITY,E_COMMODITY_SDESC",
+	  	MONTH:monthStr,
+	  	YEAR:year,
+	  	COMM_LVL:"HS10",
+	  	key: "63550916d57e686361cb2c21a3634dd765e01e28",
+	};
+	var callback2= function(data){
+		if(!data) {
+		// Nothing found, try again with previous month
+		console.log("No HS numbers found");
+		}
+		else {
+			var AllHS = data.slice(0);
+			var numcodes = AllHS.length;
+			var hscodelist = Array(numcodes);
+			var i;
+    			for (i=1;i<=numcodes;i++){
+      				hscodelist[[i-1][0]] = AllHS[[i][0]];
+      				hscodelist[[i-1][1]] = AllHS[[i][1]];
+    			}	
+		}
+	};
+	$.get(url2,expdata2,callback2);
 };
 
 function addOption(){
