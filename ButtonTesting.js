513cb470d3d7facd8dec50604267d4a5b49177b6
JeffMcHugh <Jeff.McHugh@gmail.com>
2017-03-24 11:10:37 -0400

    Update ButtonTesting.js

--- ButtonTesting.js
+++ ButtonTesting.js
@@ -1,6 +1,7 @@
 
 function addOption(){
   $(document).ready(function(){
+	gethsnumbers();
     var addop = "<option value=\"Subaru\">Subaru</option>";
     $("#menuc").append(addop);	
   });
@@ -19,5 +20,33 @@ addOption();
  		<input type="Submit">
 	</form>
 </div>  */
+
+function gethsnumbers(){
+				$.get("https://api.census.gov/data/timeseries/intltrade/exports", {
+					get: "E_COMMODITY,E_COMMODITY_SDESC",
+					YEAR: year,
+					MONTH: monthStr,
+					COMM_LVL: "HS10",
+				}, function(data) {
+					if(!data) {
+						// Nothing found, try again with previous month
+						console.log("No HS numbers found");
+					}
+					else {
+						var AllHS = data.slice(0);
+						var numcodes = AllHS.length;
+						var hscodelist = Array(numcodes);
+						var i;
+    				for (i=1;i<=numcodes;i++){
+      				hscodelist[[i-1][0]] = AllHS[[i][0]];
+      				hscodelist[[i-1][1]] = AllHS[[i][1]];
+    				}
+						
+						
+						
+					}
+				});
+}
+gethsnumbers();
 
 
