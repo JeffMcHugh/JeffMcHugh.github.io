fdd45b84b96520afdac1c61d49f4d8f33f361649
JeffMcHugh <Jeff.McHugh@gmail.com>
2017-03-25 00:45:47 -0400

    Update ButtonTesting.js

--- ButtonTesting.js
+++ ButtonTesting.js
@@ -1,7 +1,7 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    var selected = document.getElementById("myInput");
+    var selected = document.getElementById("myInput").value;
 }
 
 function filterFunction() {
