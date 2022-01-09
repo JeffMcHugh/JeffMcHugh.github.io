0494f7a64e1e817466fd4dc62856a2c831446aae
JeffMcHugh <Jeff.McHugh@gmail.com>
2022-01-09 10:11:58 -0500

    Add files via upload

--- qwiWDC2.js
+++ qwiWDC2.js
@@ -1,5 +1,5 @@
 function add() {
-  var api1 = %22+document.getElementById('api1').value;
+  var api1 = document.getElementById('api1').value;
   var api2 = document.getElementById('api2').value;
 
   var call = api1;
