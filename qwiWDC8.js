457acae7cc6d552fc876e8b9e036f8080faa86d4
JeffMcHugh <Jeff.McHugh@gmail.com>
2022-01-11 00:12:37 -0500

    Add files via upload

--- qwiWDC8.js
+++ qwiWDC8.js
@@ -3,7 +3,9 @@ function add() {
   api1text = document.getElementById('api1').value;
 
   window.localStorage.setItem("api1",api1text);
-  api1t=(window.localStorage.getItem('api1')).replace("https://","");
+  api1t=window.localStorage.getItem('api1');
+  api1t.replace("https://","")
+  h2=api1t.replace("https:","X")
 
   //Above: in this case, the `grade` is the key while `One` is the value.
 
