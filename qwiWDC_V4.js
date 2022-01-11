2b5a4e264fca351314f8b134ab3a0a3fa7ab201c
JeffMcHugh <Jeff.McHugh@gmail.com>
2022-01-11 18:43:02 -0500

    Add files via upload

---
+++ qwiWDC_V4.js
@@ -0,0 +1,148 @@
+var api1t;
+
+function add() {
+  //If you assign a value to a variable that has not been declared, it will automatically become a GLOBAL variable.
+  api1text = document.getElementById('api1').value;
+
+  window.localStorage.setItem("api1",api1text);
+  api1t=window.localStorage.getItem('api1');
+
+  //Above: in this case, the `grade` is the key while `One` is the value.
+
+  //document.getElementById("demo").innerHTML = api1text;
+  console.log("test console log")
+  console.log(api1t)
+  document.getElementById("demo").innerHTML = api1text;
+  // api url
+
+  // Defining async function
+  async function getapi(url) {
+
+    // Storing response
+    const response = await fetch(url);
+
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    numparms=window.localStorage.setItem("numparameters",data[0].length);
+    for (var i = 0, len = data[0].length-1; i < len; i++) {
+      var param="param"+i.toString();
+      window.localStorage.setItem(param,data[0][i]);
+      var paramtype=param+"type";
+      if (data[0][i]="Emp") {window.localStorage.setItem(paramtype,"string")};
+    }
+    var cols2test=[];
+      for (var i = 0, len = numparms-1; i < numparms; i++) {
+        var col={id: window.localStorage.getItem('param'+i.toString()),
+                 dataType: tableau.dataTypeEnum.string};
+        cols2test.push(col);
+      }
+  }
+  // Calling that async function
+  getapi(api1t);
+}
+
+
+
+
+(function() {
+    // Create the connector object
+    console.log('testing this too');
+    var myConnector = tableau.makeConnector();
+
+    // window.localStorage.getItem('param0')
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        numparms=window.localStorage.getItem('numparameters');
+        var cols2=[{
+          for (var i = 0, len = numparms-1; i < numparms; i++) {
+            id: window.localStorage.getItem('param'+i.toString()),
+            dataType: tableau.dataTypeEnum.string},
+          }
+        }];
+
+
+        var cols = [{
+            id: window.localStorage.getItem('param0'),
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "year",
+            alias: "year",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "quarter",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "sex",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "agegrp",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "ownercode",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "firmsize",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "seasonadj",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "industry",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "state",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "county",
+            dataType: tableau.dataTypeEnum.string
+        }];
+        console.log(cols);
+
+        var tableSchema = {
+            id: "QWIFeed",
+            alias: "QWI Data",
+            columns: cols
+        };
+
+        schemaCallback([tableSchema]);
+    };
+
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+        api1t=window.localStorage.getItem('api1');
+        $.getJSON(api1t, function(resp) {
+            //var feat = resp.features,
+            tableData = [];
+            // Iterate over the JSON object
+            for (var i = 0, len = resp.length; i < resp.length-1; i++) {
+                tableData.push({
+                    "Emp": resp[i+1][0],
+                    "year": resp[i+1][1],
+                    "quarter": resp[i+1][2],
+                    "sex": resp[i+1][3],
+                    "agegrp": resp[i+1][4],
+                    "ownercode": resp[i+1][5],
+                    "firmsize": resp[i+1][6],
+                    "seasonadj": resp[i+1][7],
+                    "industry": resp[i+1][8],
+                    "state": resp[i+1][9],
+                    "county": resp[i+1][10]
+                });
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
+
+    tableau.registerConnector(myConnector);
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "QWI Feed"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+    });
+})();
